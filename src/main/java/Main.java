4ad216c55382cf0568bc00b079c77104e3d0ca3b
linn.mann@gmail.com <linn.mann@gmail.com>
2022-01-10 19:41:38 +0100

    Added randomized parameter values

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
     Squirrel orre = new Squirrel(0.3,5,"Pine Tree Squirrel");
-    boolean gotToEat = orre.eat(true);
+    Random randBool = new Random();
+    boolean rand = randBool.nextBoolean();
+    boolean gotToEat = orre.eat(rand);
     if(gotToEat){
         System.out.println("Detta är en historia om en lycklig " + orre.getSubspecies() + " som en dag fick en god måltid:");
     }
@@ -10,12 +14,18 @@ public class Main {
         System.out.println("Detta är en  sorglig historia om en " + orre.getSubspecies() + " som en dag inte fick någon mat:");
     }
     System.out.println(orre.getStory() + "\n");
-
-    Owl owl = new Owl(23, 'F', true);
+    Random randChar = new Random();
+    char ch = randChar.nextBoolean() ? 'F' : 'M';
+    Random randInt = new Random();
+    int i = randInt.nextInt((155 - 130) + 1) + 130;
+    Owl owl = new Owl(i, ch, rand);
     owl.eat();
     System.out.println();
 
-    PineTree pt = new PineTree(80, 80, 101);
+    int randCones = randInt.nextInt(100);
+    int randAge = randInt.nextInt((150 - 50) + 1) + 50;
+    int randHeight = randInt.nextInt((110 - 80) + 1) + 80;
+    PineTree pt = new PineTree(randCones, 80, 101);
     System.out.println(pt.fall(2));
 
     }
