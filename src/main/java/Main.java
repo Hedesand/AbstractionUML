f8740ddab513af0bdb7bdfeba85f0c416c76fca0
linn.mann@gmail.com <linn.mann@gmail.com>
2022-01-10 19:03:17 +0100

    Changes to initiating class object and how class methods are called

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-    Squirrel Orre = new Squirrel(0.3,5,"Pine Tree Squirrel");
-    Orre.eat(false);
 
-    System.out.println(Orre.getStory());
+    Squirrel orre = new Squirrel(0.3,5,"Pine Tree Squirrel");
+    boolean gotToEat = orre.eat(true);
+    if(gotToEat){
+        System.out.println("Detta är en historia om en lycklig " + orre.getSubspecies() + " som en dag fick en god måltid:");
+    }
+    else{
+        System.out.println("Detta är en  sorglig historia om en " + orre.getSubspecies() + " som en dag inte fick någon mat:");
+    }
+    System.out.println(orre.getStory() + "\n");
 
-    /*PineTree pt = new PineTree(80, 80, 101);
-    System.out.println(pt.fall(2));*/
+    Owl owl = new Owl(23, 'F', true);
+    owl.eat();
+    System.out.println();
+
+    PineTree pt = new PineTree(80, 80, 101);
+    System.out.println(pt.fall(2));
 
     }
 }
