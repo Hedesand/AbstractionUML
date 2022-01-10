360f86656e47fd2b23d6d8dc48e2a783abd1b7c8
linn.mann@gmail.com <linn.mann@gmail.com>
2022-01-10 19:22:30 +0100

    wrote eat() function

--- src/main/java/Owl.java
+++ src/main/java/Owl.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Owl {
 
     private int wingSpan;
@@ -5,9 +7,8 @@ public class Owl {
     private char sex;
     private boolean alive;
 
-    Owl(int wingSpan, boolean hungry, char sex, boolean alive){
+    Owl(int wingSpan, char sex, boolean alive){
         this.wingSpan = wingSpan;
-        this.hungry = hungry;
         this.sex = sex;
         this.alive = alive;
     }
@@ -44,7 +45,65 @@ public class Owl {
         this.alive = alive;
     }
 
-    boolean eat(){
-        return true;
+    public boolean eat(){
+        if(isAlive()) {
+            Random randBool = new Random();
+            boolean rand = randBool.nextBoolean();
+            Random randCatchableSquirrels = new Random();
+            int catchableSquirrels = randCatchableSquirrels.nextInt(10);
+            if (rand) {
+                System.out.println("Ugglan är hungrig. ");
+                setHungry(true);
+            }
+            else {
+                System.out.println("Ugglan är inte hungrig. ");
+                setHungry(false);
+            }
+            if (getSex() == 'F') {
+                if (isHungry()) {
+                    System.out.println("Hon spanar ut på jakt efter en ekorre att äta.");
+                    if (catchableSquirrels > 1) {
+                        System.out.println("Hon hittar " + catchableSquirrels + " och kan äta sig mätt.");
+                        setHungry(false);
+                        return true;
+                    }
+                    else {
+                        System.out.println("Hon hittar " + catchableSquirrels + " och det är för få för att äta sig mätt. " +
+                                "\nHon dör tyvärr av svält");
+                        setAlive(false);
+                        return false;
+                    }
+                }
+                else {
+                    System.out.println("Ugglan återvänder hem utan att äta.");
+                    return false;
+                }
+            }
+            else if (getSex() == 'M') {
+                if (isHungry()) {
+                    System.out.println("Han spanar ut på jakt efter en ekorre att äta. ");
+                    if (catchableSquirrels > 2) {
+                        System.out.println("Han hittar " + catchableSquirrels + " och kan äta sig mätt.");
+                        setHungry(false);
+                        return true;
+                    }
+                    else {
+                        System.out.println("Han hittar " + catchableSquirrels + " och det är för få för att äta sig mätt. " +
+                                "\nHan dör tyvärr av svält");
+                        setAlive(false);
+                        return false;
+                    }
+                }
+                else {
+                    System.out.println("Ugglan återvänder hem utan att äta.");
+                    return false;
+
+                }
+            }
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
